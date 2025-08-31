408ea80faf4c52334d5a30e4935ec61cfe1bf0e2
sumit-mishra7777 <144814422+sumit-mishra7777@users.noreply.github.com>
2025-08-31 16:26:44 +0530

    Ensure CSS is imported

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Yeh line zaroori hai
+import './index.css' // Yeh line styles ko import karti hai
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
